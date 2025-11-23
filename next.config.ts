a3396553a514292bc087bdb75d894ae170bfc0ec
TheHaccon <matcote111@gmail.com>
2025-11-23 09:37:32 -0500

    config and page
    added output config for next on githubpage

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "out",
   reactCompiler: true,
+  basePath: "/aboutme",
+  assetPrefix: "/aboutme/",
 };
 
 export default nextConfig;
